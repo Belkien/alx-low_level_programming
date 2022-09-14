feb07a54db90c7440d3c56201e36412de1c7c844
Belkien <66989368+Belkien@users.noreply.github.com>
2022-09-15 02:02:08 +0300

    Update 0-putchar.c
    changed the code for 0putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - prints _putchar
+ * Return: 0 when successful
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char c[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
